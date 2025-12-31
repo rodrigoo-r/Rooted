8a4ff402f547671f17800e3e6e0eb084404b93be
rodrigoo-r <rodrigo@zelixlang.dev>
2025-12-31 15:03:18 -0500

    add block.h file with Block class implementation and associated methods

---
+++ include/rooted/block.h
@@ -0,0 +1,52 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 12/31/25.
+//
+
+#pragma once
+
+#include <celery/array/vector.h>
+#include <celery/string/external.h>
+
+#include "scope.h"
+
+namespace Rooted
+{
+    class Block
+    {
+        Celery::Array::Vector<bool> draw_pipe;
+        int &lines; // Passed by reference from Factory
+
+    public:
+        Block(int &lines_ref);
+
+        template<bool PrintTime, bool IsLast>
+        Scope<PrintTime> Print(Celery::Str::External &) const;
+
+        void Done() const
+        {
+            lines = 0;
+        }
+
+        ~Block()
+        {
+            Done();
+        }
+    };
+}
