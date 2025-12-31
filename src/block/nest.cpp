734c8a011bb0a6ca8710dd404b3cd862a8318583
rodrigoo-r <rodrigo@zelixlang.dev>
2025-12-31 16:47:19 -0500

    Add Nest method implementation to Block class

---
+++ src/block/nest.cpp
@@ -0,0 +1,36 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 12/31/25.
+//
+
+#include "rooted/block.h"
+
+using namespace Rooted;
+using namespace Celery;
+
+Block Block::Nest() const
+{
+    Block b(
+        lines,
+        depth + 1,
+        draw_pipe
+    );
+    draw_pipe[depth] = false;
+    return b;
+}
