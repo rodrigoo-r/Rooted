21d8b834ad9d4865b9ed63106faefb5a102ad82d
rodrigoo-r <rodrigo@zelixlang.dev>
2025-12-31 16:46:44 -0500

    Add factory.h with Factory class definition and DrawPipe member

---
+++ include/rooted/factory.h
@@ -0,0 +1,38 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 12/31/25.
+//
+
+#pragma once
+#include "block.h"
+
+namespace Rooted
+{
+    class Factory
+    {
+        DrawPipe draw_pipe;
+        int lines = 0;
+
+    public:
+        Factory() = default;
+        Block AddBlock();
+
+        ~Factory() = default;
+    };
+}
