61e1ae3c16c10cf1bc455489918b8950e71b30a1
rodrigoo-r <rodrigo@zelixlang.dev>
2025-12-31 15:03:30 -0500

    add block.cpp file with Block class constructor implementation

---
+++ src/block/block.cpp
@@ -0,0 +1,28 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly ergonomic.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the Apache License, Version 2.0.
+---------------------------------------------------------
+  - You should have received a copy of the Apache
+    License v2.0 along with Zelix. If not, see
+    <http://www.apache.org/licenses/LICENSE-2.0>.
+*/
+
+//
+// Created by rodrigo on 12/31/25.
+//
+
+#include "rooted/block.h"
+
+using namespace Rooted;
+
+Block::Block(int& lines_ref)
+    : lines(lines_ref)
+{}
