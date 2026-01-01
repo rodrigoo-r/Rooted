274bcf1ee11cd6ebc6d7988ad149b499c0a6938f
rodrigoo-r <rodrigo@zelixlang.dev>
2025-12-31 19:38:24 -0500

    Add alias for Rooted library to improve usability in CMake

--- cmake/source.cmake
+++ cmake/source.cmake
@@ -6,6 +6,7 @@ file(GLOB_RECURSE ROOTED_INTERNAL_ALL_H   "${CMAKE_CURRENT_SOURCE_DIR}/include/*
 set(ROOTED_INTERNAL_ALL_FILES ${ROOTED_INTERNAL_ALL_CPP} ${ROOTED_INTERNAL_ALL_H})
 
 add_library(Rooted STATIC ${ROOTED_INTERNAL_ALL_FILES})
+add_library(Rooted::Rooted ALIAS Rooted)
 target_include_directories(Rooted
         PUBLIC
         $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
